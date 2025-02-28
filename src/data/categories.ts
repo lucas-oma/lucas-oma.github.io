8f4dbbd005f812bad527fedef5713ce9cb2ac93f
Lucas Martinez <57833320+martinezlucas98@users.noreply.github.com>
2025-02-27 21:13:44 -0500

    Content: add Welcome blog

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,9 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
+	'Theory',
+	'Practical',
+	'GPU stuff',
+	'Discussions',
+	'Other',
 	'Coming Soon'
 ] as const
